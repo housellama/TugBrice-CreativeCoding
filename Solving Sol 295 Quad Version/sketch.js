21462672f55a5b4f0b0a2a1de1a211ef40d0e6ee
housellama <eub202@nyu.edu>
2016-09-18 16:19:18 -0400

    Homework 1 Part 1.
    Solving Sol 295. 
    Wall Drawing #295 (1976)
    
    Six white geometric figures (outlines) superimposed on a black wall.
    
    Two versions. One uses rect(), the other uses quad(). Both use a for() loop, something we haven't covered yet.

---
+++ Solving Sol 295 Quad Version/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(1000,500);
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill()
+  for(var i = 0; i < 7; i++) {
+   
+   quad(random(5,995),random(5,495),random(5,995),random(5,495),random(5,995),random(5,495),random(5,995),random(5,495));
+   
+ }
+}
+
+function draw() {
+
+  
+}
---
+++ Solving Sol 295 Rect Version/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(1000,500);
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill()
+  for(var i = 0; i < 7; i++) {
+   
+   rect(random(5,795),random(5,395),random(5,300),random(5,200));
+   
+ }
+}
+
+function draw() {
+
+  
+}
