ef5d6a2a1ea0c6dd7fc94216d042a6f2505cb0ed
Kevin Siwoff <info@kevinsiwoff.com>
2016-09-21 13:18:16 -0400

    Adds comments to Sol 056

--- Solving Sol 056/sketch.js
+++ Solving Sol 056/sketch.js
@@ -3,7 +3,13 @@ function setup() {
   createCanvas(501,501);
   stroke(0);
   strokeWeight(1);
+  //technically this will work in javascript, 
+  //but I'd recommend writing your for loops with the iterator as the last 
+  //portion of your loop header
+  // for example: for(var i=0; i < 501; i+=10)
   for(var i=0;i<501;) {
+	  //remember to always declare variables with the var keyword before variable name:
+	  // for example: var j= i + 250
 	  j=i+250;
 	  k=i+500;
 	  l=i-250;
