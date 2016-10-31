a5938ccd5164931e2320e6778f4f2be2f4bce087
Kevin Siwoff <info@kevinsiwoff.com>
2016-10-31 13:45:05 -0400

    adds comments to hw 6

--- Homework 6 Translation/sketch.js
+++ Homework 6 Translation/sketch.js
@@ -4,11 +4,14 @@ function setup() {
 }
 
 function draw() {
-  lerpColor(from,to,.25);
+ //from and to are not yet defined.  Make sure you declare and define these variables before using them
+ //try attaching the lerp amount parameter (0.25) to a changing variable.
+ lerpColor(from,to,.25);
   fill(from);
   translate(width/2+(frameCount*.01),height/2+(frameCount*.01));
   rotate(frameCount);
   background(255);
+ //instead of just lerping from pure red to pure green, try a color palette inspired by the color lecture.
   from = ('red');
   to = ('green');
   rect(-150,-150,20,20);
